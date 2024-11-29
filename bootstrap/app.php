1bf68d67977809803c568fe15b12241697dd59e4
JovannyRch <jovannyrch@gmail.com>
2024-11-29 14:14:55 -0600

    Add new middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \App\Http\Middleware\ForceHttpsMiddleware::class,
+            \App\Http\Middleware\TrustProxies::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
